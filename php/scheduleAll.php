3efd83322146ed845443856c5f019f821f72d1be
Amol <amolsw.nz@gmail.com>
2016-12-16 17:12:45 +1300

    Curl string Done Database insertion Done
    Next things check readme

--- php/scheduleAll.php
+++ php/scheduleAll.php
@@ -1,27 +1,10 @@
-<!--
-  Additional parameters set as a json string in
-  addtional Parameters json object
-    Send email to client -  true or false
-    Send email copy to me -  true or false
-    Remind me to do this -  False or the Composed reminder email text
-
-  additionalParam = {
-    sendEmail: true/false,
-    sendCopy: true/false,
-    sendReminder: false/Text
-  }
--->
 <?php
   require_once 'connect-inc.php';
 
-  // var_dump($_POST);
-
   $titleOfSchedule = array_shift($_POST);
-  // var_dump($titleOfSchedule);
 
   $arrayCounter = 0;
   $previousIndex = 1;
-  // $arrayIndex = 0;
   $insertDataArrayUnformatted = array( array());
   foreach ($_POST as $key => $value) {
     // Gets current key number eg from dateVersion_2 gives 2
@@ -29,17 +12,14 @@
     // Check if the current item belongs to same group or not
     // Done by checking previous and current index equality
     if($previousIndex != $currentIndex) {
-      // echo "<p> $currentIndex $previousIndex array(count)->$arrayCounter </p>";
       $previousIndex = $currentIndex;
       $arrayCounter++;
-      // $arrayIndex = 0;
-      // Create new array counter to add all the group variable in one array
+      // Increment array counter to add all the group variable in one array
     }
     // Removing number from the key name
     $keyNameClean = str_replace("_$currentIndex", "", $key);
     $insertDataArrayUnformatted[$arrayCounter][$keyNameClean] = $value;
   }
-  // var_dump($insertDataArrayUnformatted);
 
   $count = 0;
   $insertData = array(array());
@@ -80,37 +60,56 @@
     $extraOptions = array();
     isset($data['sendEmail']) ? $extraOptions['sendEmail'] = true : $extraOptions['sendEmail'] = false;
     isset($data['sendCopy']) ?  $extraOptions['sendCopy'] = true : $extraOptions['sendCopy'] = false;
-    isset($data['reminderText']) ? $extraOptions['reminderText'] = $data['reminderText'] : $extraOptions['reminderText'] = false;
+    if(isset($data['reminderText']) && !empty($data['reminderText']))
+      $extraOptions['reminderText'] = $data['reminderText'];
+    else
+      $extraOptions['reminderText'] = false;
     // Addtional parameters store as json string - { sendEmail: true/false, sendCopy: true/false, sendReminder: false/Text }
     $insertData[$count]['additionalParam'] = json_encode($extraOptions);
 
     $insertData[$count]['emailMsgId'] = $data['templateId'];      // emailMsgId
     $insertData[$count]['scheduleTitle'] = $titleOfSchedule;     // Every entry has same schedule title
 
+    // CURL string
+      $scheduledDate = new DateTime($insertData[$count]['specifiedDateTime']);
+
+      $minutes = (int) $scheduledDate->format('i');
+      $hours   = (int) $scheduledDate->format('H');
+      $date    = (int) $scheduledDate->format('d');
+      $month   = (int) $scheduledDate->format('m');
+
+      $curlScheduleString = "$minutes $hours $date $month * ";
+
+      $currentUrl = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' .
+                              "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+
+      // Creating send email page url wrt current folder structure
+      $sendEmailUrlArray = explode("/", $currentUrl);           // creating array from url string
+      array_pop($sendEmailUrlArray);                            // removing current fileName from the url
+      array_pop($sendEmailUrlArray);                            // going up one folder
+      array_push($sendEmailUrlArray, "cron");                   // getting inside cron job folder
+      array_push($sendEmailUrlArray, "send-mail-template.php"); // The file which needs to be executed
+      $sendEmailUrl = implode("/", $sendEmailUrlArray);         // Creating string from array
+
+      $sendEmailUrl .= "?id=" . $data['templateId'];            // Adding templateId to the url
+
+      $curlString = ' curl "' . $sendEmailUrl . '"';
+      $outputFileName = "/Applications/MAMP/htdocs/email/email/log/output.log";
+      $errorFileName = "/Applications/MAMP/htdocs/email/email/log/error.log";
+
+      $outputFile = " >> $outputFileName";
+      $errorLogFile = "2>>$errorFileName";
+      // echo "<h1>$curlScheduleString $curlString $outputFile $errorLogFile</h1>";
+      $curlScheduleString .= "$curlString $outputFile $errorLogFile";
+      echo "<h1>$curlScheduleString";
+
+    $insertData[$count]['curlString'] = $curlScheduleString;     // Every entry has same schedule title
+
     $count++; // increasing array counter for insertData varible
   }
 
-  var_dump($insertData);
-  // var_dump($insertDataArrayUnformatted);
-  // Schedule
-  //   id
-  //   scheduleTitle
-  //   emailMsgId
-  //   curlString
-  //   specifiedDateTime
-  //   additionalParam
-  //   status - "Active" / "Inactive"
-  //   dateCreated
-  //   dateUpdated
-  // $sql = "INSERT INTO Schedule (emailMsgId, scheduleTitle, curlString, specifiedDateTime, additionalParam) VALUES (?, ?, ?, ?, ?)";
-  // $stmt = $pdo->prepare($sql);
-  // $stmt->bindValue(1, $_POST['templateId']);
-  // $stmt->bindValue(2, $_POST['title']);
-  // $stmt->bindValue(3, "$curlScheduleString $curlString $outputFile $errorLogFile");
-  // $stmt->bindValue(4, "X");
-  // $stmt->execute();
-  // echo json_encode(array("SUCCESS", "Email job scheduled"));
-  $dataFields =
+  // var_dump($insertData);
+  $dataFields = array_keys($insertData[0]);
   array_keys($insertData[0] );
   $pdo->beginTransaction();
       $dataToInsert = array();
